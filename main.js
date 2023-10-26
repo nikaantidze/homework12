6eaadd0e74b646401a414b456cb4d5eeb3d6fbc0
nikaantidze <nikaantidze33@gmail.com>
2023-10-26 19:46:04 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const brgr = document.querySelector('.brgr')
+const brgrDiv = document.querySelector('.brgr123')
+
+brgr.addEventListener('click', () => {
+    brgrDiv.classList.toggle('damaluli')
+})
